cc4e7b53df607e7aca860e960a7722a65126e2cd
root <root@sd-63455.dedibox.fr>
2015-03-24 23:57:14 +0100

    arreglos visuales

--- constantes.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
-*@author bauerpy
- */
-    // Base de datos.
-    define("BD", "dncp_visualizacion");
-    define("HOST", "localhost");
-    define("USUARIO", "root");
-    define("PASS", "145541");
